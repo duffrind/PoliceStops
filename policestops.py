8cf418c6191d8618226c8e7e284587fc6c00b551
David Duffrin <david.duffrin15@ncf.edu>
2017-11-06 22:32:50 -0500

    heroku

---
+++ policestops.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+import logging
+import json
+import os
+from app.views import app as application
+
+logging.basicConfig(stream=sys.stderr)
+application.secret_key = 'whatislove'
+
+ if __name__ == '__main__':
+     app.debug = True
+     port = int(os.environ.get("PORT", 5000))
+     app.run(host='0.0.0.0', port=port)
--- run.py
+++ run.py
@@ -1,3 +1,5 @@
 #!/Users/duffrind/miniconda3/bin/python
 from app import app
 app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
